6702ff72a3993bc01c0a244414cb75d982325d1d
Gabriel Ribeiro <gabrielkaina99@gmail.com>
2023-02-15 13:39:15 +0000

    highlight component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { BackIcon, Container, Logo, BackButton } from './styles';
+import logoImg from '@assets/logo.png';
+
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export const Header = ({ showBackButton = false }: HeaderProps) => {
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+      <Logo source={logoImg} />
+    </Container>
+  );
+};
